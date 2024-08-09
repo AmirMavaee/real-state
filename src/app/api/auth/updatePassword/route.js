5dbff3a94abb263822f09fec3e5f31869b58af8c
Amir Mavaee <ah.mavaee3831@gmail.com>
2024-08-09 01:16:15 -0700

    add updatePassword api

---
+++ src/app/api/auth/updatePassword/route.js
@@ -0,0 +1,34 @@
+import User from "@/models/User";
+import { hashPassword } from "@/utils/auth";
+import connectDB from "@/utils/connectDB";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+import { authOptions } from "../[...nextauth]/route";
+
+export async function mongodb(req) {
+  try {
+    await connectDB();
+
+    const session = await getServerSession(authOptions);
+
+    const { password } = await req.json();
+    
+    const hashPass = await hashPassword(password);
+
+    await User.findOneAndUpdate(
+      { email: session.user.email },
+      { password: hashPass }
+    );
+
+    return NextResponse.json(
+      { message: "رمز عبور با موفقیت تغییر پیدا کرد" },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(
+      { error: "مشکلی در سرور رخ داده است" },
+      { status: 500 }
+    );
+  }
+}
