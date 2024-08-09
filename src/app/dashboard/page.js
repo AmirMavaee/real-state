f55fee04213e1538509d23cadd904a7ff3bcc955
Amir Mavaee <ah.mavaee3831@gmail.com>
2024-08-09 01:47:02 -0700

    add connectdb in dashboard page in app folder

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,12 +1,14 @@
 import User from '@/models/User';
+import connectDB from '@/utils/connectDB';
 import { getServerSession } from 'next-auth'
 import React from 'react'
 import DashboardPage from '../../components/templates/DashboardPage'
 import { authOptions } from '../api/auth/[...nextauth]/route'
 
 async function dashboard() {
+  await connectDB();
   const session = await getServerSession(authOptions);
-  const user = await User.findOne({ email : session?.user?.email})
+  const user = await User.findOne({ email : session.user.email})
   return (
     <DashboardPage date={user.createdAt}/>
   )
